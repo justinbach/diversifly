808d9d58434a3d0ce4ae2dbeee61ccd337c7e7d3
Justin Bachorik <justinbach@gmail.com>
2013-05-10 15:31:24 -0400

    added 5-color data file

--- inc/footer.php
+++ inc/footer.php
@@ -3,6 +3,7 @@
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
     <script src="js/vendor/jquery-ui-1.10.3.custom.min.js"></script>
     <script src="<?php echo $abs_path ?>js/plugins.js"></script>
+    <script src="<?php echo $$abs_path?>js/data.js"></script>
     <script src="<?php echo $abs_path ?>js/main.js"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
