f25bc6f58141ef6a7a264f7547fb1d5a509fbab6
Justin Bachorik <justinbach@gmail.com>
2013-05-13 17:03:29 -0400

    layout of palette cards is in place

--- inc/templates/palette.php
+++ inc/templates/palette.php
@@ -1,8 +1,37 @@
 <script type="text/template" id="palette-template">
-  <div id="palette-goal" class="goal"></div>
+  <!-- <div id="palette-goal" class="goal"></div> -->
   <div id="palette" class="page-container">
     <img src="img/diversifly_logo.svg" width="180" height="134" />
     <h1 class="banner">SELECT COLOR PALETTE</h1>
     <div class="spacer"></div>
+    <div class="palette-page">
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+      <a href="#" class="butterfly-palette">
+        <img src="img/palette.php" width="296" height="196"/>
+      </a>
+    </div>
+    </div>
+    </div>
+    </div>
   </div>
 </script>
--- js/main.js
+++ js/main.js
@@ -102,7 +102,7 @@ $("document").ready(function() {
 
   }
 
-  navigateTo('landing');
+  navigateTo('palette'); // kick things off
 
 
 });
