99a0e8ac2c27c4eddbe2538725398b31d72c92ef
Justin Bachorik <justinbach@gmail.com>
2013-05-13 08:38:21 -0400

    got rid of pages

--- pages/landing.php
+++
@@ -1,9 +0,0 @@
-<!-- <div id="landing-goal" class="goal"></div> -->
-<div id="landing" class="page-container">
-  <img src="img/diversifly_logo.svg" width="336" height="250" />
-  <p>WELCOME TO DIVERSIFLY</p>
-  <div class="buttons">
-    <a class="button" href="<?php echo get_link("eyespots")?>"><span>Get Started</span><img src="img/eye_button.php?color=B82025" width="186" height="190"></a>
-    <a class="button" href="<?php echo get_link("about")?>"><span>Learn More</span><img src="img/eye_button.php?color=D6A86F" width="186" height="190"></a>
-  </div>
-</div>
--- pages/palette.php
+++
@@ -1,6 +0,0 @@
-<div id="palette-goal" class="goal"></div>
-<div id="palette" class="page-container">
-  <img src="img/diversifly_logo.svg" width="180" height="134" />
-  <h1 class="banner">SELECT COLOR PALETTE</h1>
-  <div class="spacer"></div>
-</div>
