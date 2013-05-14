7d09d5477e39b0bf749f4b0e73a2bc73a0de9e1b
Justin Bachorik <justinbach@gmail.com>
2013-05-14 11:20:20 -0400

    tried global app object, going to make it extend backbone

--- js/main.js
+++ js/main.js
@@ -44,100 +44,137 @@ $(function() {
 
   var oldRoute = "";
 
-  // routing via backbone
-  var Router = Backbone.Router.extend({
-    routes : {
-      "palette/eyespots/:spotCount/page/:page" : "palette",
-      "eyespots" : "eyespots",
-      ":page" : "defaultTransition",
-      "*path" : "defaultRoute"
-    },
+  // // routing via backbone
+  // var Router = Backbone.Router.extend({
+  //   routes : {
+  //     "palette/eyespots/:spotCount/page/:page" : "palette",
+  //     "eyespots" : "eyespots",
+  //     ":page" : "defaultTransition",
+  //     "*path" : "defaultRoute"
+  //   },
 
-    // the eyespots page
-    eyespots: function () {
-      var init = function() {
-        var SliderView = Backbone.View.extend({
-          initialize: function() {
-            _.bindAll(this, 'render');
-            this.model.bind('change:eyespot_count', this.render);
-            this.$handleText = null;
-            this.$eyespotSlider = $('#eyespot-slider');
-            this.render();
-          },
-          render: function() {
-            var v = this;
-            var m = this.model;
-            v.$eyespotSlider.slider({
-              value: m.get('eyespot_count'),
-              min: 1,
-              max: 18,
-              step: 1,
-              create: function() {
-                $('.ui-slider-handle').first().html(
-                  '<span id="handle-text">'+m.get('eyespot_count')+'</span>'
-                );
-                v.$handleText = $('#handle-text');
-              },
-              slide: function(event, ui) {
-                m.set({'eyespot_count': ui.value});
-              }
-            });
-            this.$handleText.html(m.get('eyespot_count'));
-          }
-        });
+  //   // the eyespots page
+  //   eyespots: function () {
+  //     var init = function() {
+  //       var SliderView = Backbone.View.extend({
+  //         initialize: function() {
+  //           _.bindAll(this, 'render');
+  //           this.model.bind('change:eyespot_count', this.render);
+  //           this.$handleText = null;
+  //           this.$eyespotSlider = $('#eyespot-slider');
+  //           this.render();
+  //         },
+  //         render: function() {
+  //           var v = this;
+  //           var m = this.model;
+  //           v.$eyespotSlider.slider({
+  //             value: m.get('eyespot_count'),
+  //             min: 1,
+  //             max: 18,
+  //             step: 1,
+  //             create: function() {
+  //               $('.ui-slider-handle').first().html(
+  //                 '<span id="handle-text">'+m.get('eyespot_count')+'</span>'
+  //               );
+  //               v.$handleText = $('#handle-text');
+  //             },
+  //             slide: function(event, ui) {
+  //               m.set({'eyespot_count': ui.value});
+  //             }
+  //           });
+  //           this.$handleText.html(m.get('eyespot_count'));
+  //         }
+  //       });
 
-        var sliderView = new SliderView({
-          model: butterfly
-        });
+  //       var sliderView = new SliderView({
+  //         model: butterfly
+  //       });
 
-        // bind the button to the eyespot count for linking
-        var PaletteButtonView = Backbone.View.extend({
-          initialize: function() {
-            _.bindAll(this, 'updateLink');
-            this.model.bind('change:eyespot_count', this.updateLink);
-          },
-          updateLink : function () {
-            this.el.setAttribute("href", "palette/eyespots/" + this.model.get('eyespot_count')
-              + "/page/1");
-          }
-        });
+  //       // bind the button to the eyespot count for linking
+  //       var PaletteButtonView = Backbone.View.extend({
+  //         initialize: function() {
+  //           _.bindAll(this, 'updateLink');
+  //           this.model.bind('change:eyespot_count', this.updateLink);
+  //         },
+  //         updateLink : function () {
+  //           this.el.setAttribute("href", "palette/eyespots/" + this.model.get('eyespot_count')
+  //             + "/page/1");
+  //         }
+  //       });
 
-        var paletteButtonView = new PaletteButtonView({
-          model: butterfly,
-          el: document.getElementById('palette-link')
-        });
-      };
-      this.defaultTransition('eyespots', init);
-    },
+  //       var paletteButtonView = new PaletteButtonView({
+  //         model: butterfly,
+  //         el: document.getElementById('palette-link')
+  //       });
+  //     };
+  //     this.defaultTransition('eyespots', init);
+  //   },
 
-    palette : function () {
-      this.defaultTransition('palette');
-    },
+  //   palette : function () {
+  //     this.defaultTransition('palette');
+  //   },
 
-    defaultRoute : function() {
-      this.defaultTransition('home');
-    },
+  //   defaultRoute : function() {
+  //     this.defaultTransition('home');
+  //   },
 
-    defaultTransition : function(page, init) {
-      console.log("from", oldRoute);
-      console.log("to", Backbone.history.fragment);
+  //   defaultTransition : function(page, init) {
+  //     console.log("from", oldRoute);
+  //     console.log("to", Backbone.history.fragment);
 
-      $pageHolder.animateOut();
-      $pageHolder.promise().done(function() {
-        // load new content, execute page functions,
-        // and bind click handlers
-        $pageHolder.html($("#" + page + "-template").html());
-        if(typeof init != "undefined")
-          init();
-        bindNavLinks();
-        $pageHolder.animateIn();
-      });
-      oldRoute = page;
-    }
+  //     $pageHolder.animateOut();
+  //     $pageHolder.promise().done(function() {
+  //       // load new content, execute page functions,
+  //       // and bind click handlers
+  //       $pageHolder.html($("#" + page + "-template").html());
+  //       if(typeof init != "undefined")
+  //         init();
+  //       bindNavLinks();
+  //       $pageHolder.animateIn();
+  //     });
+  //     oldRoute = page;
+  //   }
+  // });
+
+  // var router = new Router();
+  // Backbone.history.start(); // let's go
+
+
+  var SimpleView = Backbone.View.extend({
+    render:
   });
 
-  var router = new Router();
-  Backbone.history.start(); // let's go
+  var HomeView = Backbone.View.extend({ template: _.template($('#home-template').html())});
+  var AboutView = Backbone.View.extend({ template: _.template($('#about-template').html())});
+  var EyespotsView = Backbone.View.extend({ template: _.template($('#eyespots-template').html())});
+  var PaletteView = Backbone.View.extend({ template: _.template($('#palette-template').html())});
+
+
+  var App = {
+    initialize : function () {
+      App.views = {
+        homeView : new HomeView(),
+        aboutView : new AboutView(),
+        eyespotsView : new EyespotsView(),
+        paletteView : new PaletteView()
+      };
+      $pageHolder = $('#page-holder');
+      debugger;
+      $pageHolder.append(App.views.homeView.el);
+
+    },
+
+    showView : function (view) {
+      if (App.views.current != undefined) {
+        $(App.views.current.el).hide();
+      }
+      App.views.current = view;
+      $(App.views.current.el).show();
+    }
+  }
+
+  App.initialize();
+  App.showView(App.views.homeView);
 
 
 
