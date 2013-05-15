fe7d33cdd45d7296970adc9590496c3a816b8747
Justin Bachorik <justinbach@gmail.com>
2013-05-15 08:13:59 -0400

    new palette cards in place, layout slightly adjusted

--- inc/templates/palette.php
+++ inc/templates/palette.php
@@ -1,5 +1,5 @@
 <script type="text/template" id="palette-template">
-  <!-- <div id="palette-goal" class="goal"></div> -->
+  <div id="palette-goal" class="goal"></div>
   <div id="palette" class="page-container">
     <img src="img/diversifly_logo.svg" width="180" height="134" />
     <h1 class="banner">SELECT COLOR PALETTE</h1>
