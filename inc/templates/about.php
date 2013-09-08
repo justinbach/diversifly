90e601daeac083aea95295847163948145c35205
Justin Bachorik <justinbach@gmail.com>
2013-09-08 11:35:48 -0400

    copy in place

--- inc/templates/about.php
+++ inc/templates/about.php
@@ -4,10 +4,10 @@
     <img src="img/diversifly_logo.png" width="180" height="134" />
     <h1 class="banner">ABOUT DIVERSIFLY</h1>
     <div class="spacer"></div>
-    <h2 class="subtitle">AT VERO EOS OFFICIAS ET ACCUSAMUS ET IUSTO ODIO DIGNISSIMOS SET AMETITATE DOLOR DUCIMUS QUI BLANDITIIS PRAESENTIUM ET AMET VOLUPTATUM DELENITI ATQUE CORRUPTI QUOS DOLORES ET QUAS.</h2>
+    <h2 class="subtitle">Butterflies are very diverse! Some are blue, some are yellow, some are orange, and some are several different colors combined.  Many butterflies also have spots on their wings that are called "eye spots" because they often look like eyes. Diversifly lets you explore the diversity of real butterflies with different numbers of eyespots and color combinations.</h2>
     <div class="spacer"></div>
     <p class="body-copy">
-      Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor set. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae estra sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis.
+   Why have different butterflies evolved different colors and different numbers of eye spots? Sometimes butterflies are brightly colored to warn predators that they are poisonous. In other cases, butterflies are colorful and have eye spots because these traits make them attractive to the opposite sex. Some butterflies distract predators with their eye spots to avoid getting eaten. Butterflies may also startle predators by flashing their brightly colored and spotted wings, giving them time to escape.
     </p>
     <a class="button nav red" href="eyespots"><span>Get Started</span></a>
   </div>
